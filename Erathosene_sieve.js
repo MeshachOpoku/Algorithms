85284558d2e7c05a018614cdb9a3f3b9765ca44f
MeshachOpoku <104779012+MeshachOpoku@users.noreply.github.com>
2025-07-31 01:40:48 +0000

    Update Erathosene_sieve.js
    Update

--- Erathosene_sieve.js
+++ Erathosene_sieve.js
@@ -5,37 +5,45 @@
 //array defined and assign
 arr=[]
 
-// get numbers from 2 to 25
+//start: get numbers from 2 to 1000000 
 total_num=1000000 
 for (i=2;i<=total_num;i++){ arr.push(i)}
 console.log(arr)
+//end: get numbers from 2 to 1000000 
 
 
 
 
-divisor=0
+divisor=0  // is an index  for divisor  element in arr 
 
-//Take only prime numbers
+
+//start :Take only prime numbers
 do{
-    for (start=1;start<=arr.length; start+=1){
+    for (start=1;start<=arr.length;start+=1){
       if(arr[start]%arr[divisor]===0){
-             if (arr[divisor]===arr[start]){continue}
-             else {arr[start]=0;} 
+              if (arr[divisor]===arr[start]){continue} // dont assign zero for primes,  given a/b then a===b if true skip 
+              else {arr[start]=0;}  // assign zero for non-prime numbers
      
-     }
+       }
    
     }
     
-   start+=1; 
+   start+=1;  // changing the divisor index
    value=0; 
-arr=arr.filter(data=>data!==value);// remove all zero
+arr=arr.filter(data=>data!==value);// remove all zero and keep only primes
 
-divisor+=1; //changing the index d
+divisor+=1; //changing the index of divisor
 
  decoder=(3/100)*(total_num)
 }while(divisor<=decoder)
+//end:Take only prime numbers
 
 
+//display  the output: arr and its contents
 console.log(arr)
+
+//display number of element in the arr 
 console.log(arr.length)
 
+
+//if you still donot understand : send me mail on :meshachboadi230@gmail.com
